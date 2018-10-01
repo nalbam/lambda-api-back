40aeb958eca49488ab2e66a07e028eca1492f7e9
nalbam <me@nalbam.com>
2018-10-01 19:15:39 +0900

    dynamodb

--- main.tf
+++ main.tf
@@ -38,6 +38,8 @@ module "dev-api-back" {
   certificate_arn = "${module.domain.certificate_arn}"
   domain_name     = "${var.name}-${var.stage}.${var.domain}"
 
+  dynamodb = "true"
+
   env_vars = {
     PROFILE = "${var.stage}"
   }
