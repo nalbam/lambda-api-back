b2e78d63fadc853c6562b1c093ebd44a7561c557
nalbam <me@nalbam.com>
2019-05-30 23:32:04 +0900

    lint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const doc = require('dynamodb-doc')
-    , dynamo = new doc.DynamoDB()
-    , debug = true;
+const doc = require('dynamodb-doc'),
+    dynamo = new doc.DynamoDB(),
+    debug = true;
 
 if (!debug) {
-    console.log = () => {
-    };
-    console.error = () => {
-    };
+    console.log = () => {};
+    console.error = () => {};
 }
 
 exports.handler = (event, context, callback) => {
@@ -18,31 +16,31 @@ exports.handler = (event, context, callback) => {
     const payload = event.payload;
 
     switch (operation) {
-    case 'put':
-        dynamo.putItem(payload, callback);
-        break;
-    case 'get':
-        dynamo.getItem(payload, callback);
-        break;
-    case 'update':
-        dynamo.updateItem(payload, callback);
-        break;
-    case 'delete':
-        dynamo.deleteItem(payload, callback);
-        break;
-    case 'scan':
-        dynamo.scan(payload, callback);
-        break;
-    case 'query':
-        dynamo.query(payload, callback);
-        break;
-    case 'echo':
-        callback(null, payload);
-        break;
-    case 'ping':
-        callback(null, 'pong');
-        break;
-    default:
-        callback(new Error(`Unrecognized operation "${operation}"`));
+        case 'put':
+            dynamo.putItem(payload, callback);
+            break;
+        case 'get':
+            dynamo.getItem(payload, callback);
+            break;
+        case 'update':
+            dynamo.updateItem(payload, callback);
+            break;
+        case 'delete':
+            dynamo.deleteItem(payload, callback);
+            break;
+        case 'scan':
+            dynamo.scan(payload, callback);
+            break;
+        case 'query':
+            dynamo.query(payload, callback);
+            break;
+        case 'echo':
+            callback(null, payload);
+            break;
+        case 'ping':
+            callback(null, 'pong');
+            break;
+        default:
+            callback(new Error(`Unrecognized operation "${operation}"`));
     }
 };
